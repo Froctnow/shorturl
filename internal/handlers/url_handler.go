75a8921734d4d9fea3ecfbb43527b332e45d3df8
Ivan <froctnow@gmail.com>
2023-12-03 00:10:53 +0600

    iter-1 Поменял regexp для URL

--- internal/handlers/url_handler.go
+++ internal/handlers/url_handler.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-const RegexpURL = "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/|\\/|\\/\\/)?[A-z0-9_-]*?[:]?[A-z0-9_-]*?[@]?[A-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$"
+const RegexpURL = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)"
 
 type URLHandler struct {
 	urlService *services.URLService
