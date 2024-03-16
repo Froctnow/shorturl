84171d30f98707229fa2ef7bc0e291bd8ac3864e
Ivan <froctnow@gmail.com>
2024-03-16 14:07:19 +0600

    Сделал iter5

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -3,36 +3,42 @@ package config
 import (
 	"flag"
 	"fmt"
-)
-
-const (
-	AppBuildRelease = "release"
+	"github.com/caarlos0/env/v6"
 )
 
 type Values struct {
-	Address  string
-	Hostname string
+	Address  string `env:"SERVER_ADDRESS" envSeparator:":"`
+	Hostname string `env:"BASE_URL" envSeparator:":"`
 }
 
 func NewConfig() (*Values, error) {
+	var cfg Values
 	address := flag.String("a", "", "address of service")
 	hostname := flag.String("b", "", "hostname of service")
 
-	if *address == "" {
-		*address = fmt.Sprintf(`:%d`, 8080)
-	}
-	if *hostname == "" {
-		*hostname = "http://localhost:8080"
+	err := env.Parse(&cfg)
+
+	if err != nil {
+		panic(fmt.Errorf("can't parse env %w", err))
 	}
 
 	// разбор командной строки
 	flag.Parse()
-	cfg := &Values{
-		Address:  *address,
-		Hostname: *hostname,
+
+	if cfg.Address == "" {
+		if *address == "" {
+			*address = fmt.Sprintf(`:%d`, 8080)
+		}
+
+		cfg.Address = *address
+	}
+	if cfg.Hostname == "" {
+		if *hostname == "" {
+			*hostname = "http://localhost:8080"
+		}
+
+		cfg.Hostname = *hostname
 	}
 
-	fmt.Println(cfg)
-
-	return cfg, nil
+	return &cfg, nil
 }
