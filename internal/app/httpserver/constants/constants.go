a390b605bb5b867a89535b6ad72d924e980958e3
Ivan <froctnow@gmail.com>
2024-08-10 16:02:57 +0300

    Поправил regexp для автотестов

--- internal/app/httpserver/constants/constants.go
+++ internal/app/httpserver/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	RegexpURL        = "(https?:\\/\\/)?[0-9a-zA-Z]+\\.[-_0-9a-zA-Z]+\\.[0-9a-zA-Z]+"
+	RegexpURL        = "(http[s]?:\\/\\/)?([^\\/\\s]+\\/)(.*)"
 	ContextUserID    = "user_id"
 	ContextIsNewUser = "is_new_user"
 )
